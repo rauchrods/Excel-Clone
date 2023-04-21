ee1a92400d6dfa94887d7aff7929b5ae6302a798
rauch <rauchrodrigues@gmail.com>
2023-04-21 17:52:03 +0530

    2nd commit

--- script.js
+++ script.js
@@ -28,212 +28,260 @@ let borderleftbtn = document.querySelector("#border-left-btn");
 let borderrightbtn = document.querySelector("#border-right-btn");
 let borderouterbtn = document.querySelector("#border-outer-btn");
 
+let downloadbtn = document.querySelector("#download-btn");
+
 
 let addsheetbtn = document.querySelector("#add-sheet-btn");
-let sheet_no=2;
+let sheet_no = 2;
 
-let cutobj={};
+let cutobj = {};
 
- let tr = document.createElement("tr");
- let th = document.createElement("th");
- th.innerText = "◢";
- th.style.fontSize = "15px";
- tr.appendChild(th);
+let tr = document.createElement("tr");
+let th = document.createElement("th");
+th.innerText = "◢";
+th.style.fontSize = "15px";
+tr.appendChild(th);
 
 
-for(let col = 0; col< 26; col++) {
+for (let col = 0; col < 26; col++) {
     let th = document.createElement("th");
-    th.innerText = String.fromCharCode(col+65);
+    th.innerText = String.fromCharCode(col + 65);
     tr.appendChild(th);
 }
 
 thead.appendChild(tr);
- 
 
-for(let row = 0; row< 150; row++) {
+
+for (let row = 0; row < 100; row++) {
 
     let tr = document.createElement("tr");
     let th = document.createElement("th");
-    th.innerText=(row+1);
+    th.innerText = (row + 1);
     tr.appendChild(th);
 
-    
-     for(let col = 0; col< 26; col++) {
+
+    for (let col = 0; col < 26; col++) {
         let td = document.createElement("td");
         td.setAttribute("contenteditable", true);
         td.setAttribute("onfocus", "getcurrentcell(event)");
+        td.setAttribute("oninput", "getcurrentcell(event)");
         td.setAttribute("spellcheck", false);
-        td.setAttribute("id", `${String.fromCharCode(col+65)}${row+1}`);
+        td.setAttribute("id", `${String.fromCharCode(col + 65)}${row + 1}`);
         tr.appendChild(td);
-     }
+    }
 
 
     tbody.appendChild(tr);
 }
 
+// making matrix data to store all the information of every cell
+let matrixdata = new Array(100);
+
+for (let i = 0; i < 100; i++) {
+    matrixdata[i] = new Array(26);
+    for (let j = 0; j < 26; j++) {
+        matrixdata[i][j] = {};
+    }
+}
+
+// console.log(matrixdata);
+
+function updateJSON(cell) {
+    let obj = {
+        text: cell.innerText,
+        style: cell.style.cssText,
+        id: cell.getAttribute("id")
+    }
+
+    let row = Number(cell.id.substring(1)) - 1;
+    let column = cell.id.charCodeAt(0) - 65;
+    //  console.log(row, column);
+    matrixdata[row][column] = obj;
+
+    console.log(matrixdata);
+}
 
 let currele;
 
-function getcurrentcell(event){
- currele =  event.target;
- let myid = currele.getAttribute("id");
- currposooutput.value = myid;
- console.log(currele);
+function getcurrentcell(event) {
+    currele = event.target;
+    let myid = currele.getAttribute("id");
+    currposooutput.value = myid;
+    console.log(currele);
 
-
-//   fontsizebtn.value = currele.style.fontSize;
+    updateJSON(currele);
+    //   fontsizebtn.value = currele.style.fontSize;
 }
 
-boldbtn.addEventListener("click",()=>{
+boldbtn.addEventListener("click", () => {
 
-    if(currele.style.fontWeight == "bold"){
+    if (currele.style.fontWeight == "bold") {
         currele.style.fontWeight = "normal";
     }
-    else{
+    else {
         currele.style.fontWeight = "bold";
     }
 
     console.log("bold clicked");
+    updateJSON(currele);
 });
 
-italicbtn.addEventListener("click",()=>{
+italicbtn.addEventListener("click", () => {
 
-    if(currele.style.fontStyle == "italic"){
+    if (currele.style.fontStyle == "italic") {
         currele.style.fontStyle = "normal";
     }
-    else{
+    else {
         currele.style.fontStyle = "italic";
     }
     console.log("italic clicked");
+    updateJSON(currele);
 });
 
-underlinebtn.addEventListener("click",()=>{
+underlinebtn.addEventListener("click", () => {
 
-    if(currele.style.textDecoration == "underline"){
+    if (currele.style.textDecoration == "underline") {
         currele.style.textDecoration = "none";
     }
-    else{
+    else {
         currele.style.textDecoration = "underline";
     }
     console.log("underline clicked");
+    updateJSON(currele);
 });
 
-txtcolorbtn.addEventListener("input",()=>{
+txtcolorbtn.addEventListener("input", () => {
 
     currele.style.color = txtcolorbtn.value;
 
     console.log("txt-color active");
+    updateJSON(currele);
 });
 
-fillcolorbtn.addEventListener("input",()=>{
+fillcolorbtn.addEventListener("input", () => {
 
     currele.style.backgroundColor = fillcolorbtn.value;
 
     console.log("txt-color active");
+    updateJSON(currele);
 });
 
-leftalignbtn.addEventListener("click",()=>{
+leftalignbtn.addEventListener("click", () => {
 
     currele.style.textAlign = "left";
+    updateJSON(currele);
 
     // console.log("left-alignbtn active");
 });
 
-centeralignbtn.addEventListener("click",()=>{
+centeralignbtn.addEventListener("click", () => {
     currele.style.textAlign = "center";
     console.log("center-alignbtn active");
+    updateJSON(currele);
 });
 
 
-rightalignbtn.addEventListener("click",()=>{
+rightalignbtn.addEventListener("click", () => {
     currele.style.textAlign = "right";
     console.log("right-alignbtn active");
+    updateJSON(currele);
 });
 
-fontsizebtn.addEventListener("change",()=>{
+fontsizebtn.addEventListener("change", () => {
     currele.style.fontSize = `${fontsizebtn.value}px`;
-    
+    updateJSON(currele);
+
 });
 
-fontstylebtn.addEventListener("change",()=>{
+fontstylebtn.addEventListener("change", () => {
     // console.log(f);
     currele.style.fontFamily = fontstylebtn.value;
-    
+    updateJSON(currele);
+
 });
 
-cutbtn.addEventListener("click",()=>{
-    cutobj={
+cutbtn.addEventListener("click", () => {
+    cutobj = {
         innertxt: currele.innerText,
         styles: currele.style.cssText
     }
-    currele.innerText="";
-    currele.style="";
+    currele.innerText = "";
+    currele.style = "";
     console.log(cutobj);
-    
+
+    updateJSON(currele);
+
 });
 
-copybtn.addEventListener("click",()=>{
-    cutobj={
+copybtn.addEventListener("click", () => {
+    cutobj = {
         innertxt: currele.innerText,
         styles: currele.style.cssText
     }
 
     console.log(cutobj);
-    
+
 });
 
-pastebtn.addEventListener("click",()=>{
+pastebtn.addEventListener("click", () => {
 
     currele.innerText = cutobj.innertxt;
     currele.style = cutobj.styles;
+
+    updateJSON(currele);
 });
 
-borderbtmbtn.addEventListener("click",()=>{
-    if(currele.style.borderBottom == "2px solid black"){
+borderbtmbtn.addEventListener("click", () => {
+    if (currele.style.borderBottom == "2px solid black") {
         currele.style.borderBottom = "1px solid lightgray";
     }
-    else{
+    else {
         currele.style.borderBottom = "2px solid black";
-    }    
+    }
+    updateJSON(currele);
 });
 
-bordertopbtn.addEventListener("click",()=>{
-    if(currele.style.borderTop == "2px solid black"){
+bordertopbtn.addEventListener("click", () => {
+    if (currele.style.borderTop == "2px solid black") {
         currele.style.borderTop = "1px solid lightgray";
     }
-    else{
+    else {
         currele.style.borderTop = "2px solid black";
-    }    
+    }
+    updateJSON(currele);
 });
 
-borderleftbtn.addEventListener("click",()=>{
-    if(currele.style.borderLeft == "2px solid black"){
+borderleftbtn.addEventListener("click", () => {
+    if (currele.style.borderLeft == "2px solid black") {
         currele.style.borderLeft = "1px solid lightgray";
     }
-    else{
+    else {
         currele.style.borderLeft = "2px solid black";
-    }    
+    }
+    updateJSON(currele);
 });
 
-borderrightbtn.addEventListener("click",()=>{
-    if(currele.style.borderRight == "2px solid black"){
+borderrightbtn.addEventListener("click", () => {
+    if (currele.style.borderRight == "2px solid black") {
         currele.style.borderRight = "1px solid lightgray";
     }
-    else{
+    else {
         currele.style.borderRight = "2px solid black";
-    }    
+    }
+    updateJSON(currele);
 });
 
-borderouterbtn.addEventListener("click",()=>{
-    if(currele.style.border == "2px solid black"){
+borderouterbtn.addEventListener("click", () => {
+    if (currele.style.border == "2px solid black") {
         currele.style.border = "1px solid lightgray";
     }
-    else{
+    else {
         currele.style.border = "2px solid black";
-    }    
+    }
+    updateJSON(currele);
 });
 
-addsheetbtn.addEventListener("click",()=>{
+addsheetbtn.addEventListener("click", () => {
     let sheetdiv = document.querySelector(".sheet-add");
     let sheettab = document.createElement("span");
     sheettab.innerText = `Sheet ${sheet_no}`;
@@ -242,5 +290,68 @@ addsheetbtn.addEventListener("click",()=>{
 });
 
 
+function downloadJSON() {
+
+    //need to be converted to string format
+    let jsonmatrixstring = JSON.stringify(matrixdata);
+
+    let blob = new Blob([jsonmatrixstring], { type: "application/json" });
+
+    let link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+
+    link.download = "matrixdata.json"; // filename by which this will be downloaded
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+     
+}
 
 
+downloadbtn.addEventListener("click", downloadJSON);
+
+
+document.querySelector("#upload-btn").addEventListener("change", (event) => {
+    let file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const fileContent = e.target.result;
+
+            // {id,style,text}
+            // Parse the JSON file content and process the data
+            try {
+                const jsondata = JSON.parse(fileContent);
+                console.log(jsondata);
+
+                printExcelSheet(jsondata);
+
+            } catch (error) {
+                console.error("Error parsing JSON file:", error);
+            }
+        };
+
+        reader.readAsText(file);
+    }
+});
+
+
+function printExcelSheet(jsondata) {
+
+
+    jsondata.forEach((row) => {
+        row.forEach((col) => {
+            if (col.id) {
+                let cell = document.getElementById(`${col.id}`)
+                cell.innerText = col.text;
+                cell.style.cssText = col.style;
+            }
+
+        });
+    });
+}
